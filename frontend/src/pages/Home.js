3c5bfd8f1bc1360225ed9ff1d8e1ce1cf6984fce
Ayush <ayushbangwal0@gmail.com>
2026-03-10 17:02:07 +0530

    fix players api response

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -29,8 +29,8 @@ const Home = () => {
         playerAPI.getSportsStats()
       ]);
       
-      setTopPlayers(topPlayersRes.data.data);
-      setSportsStats(statsRes.data.stats);
+      setTopPlayers(topPlayersRes?.data?.players || []);
+      setSportsStats(statsRes?.data?.stats || {} );
     } catch (error) {
       console.error('Error fetching data:', error);
     } finally {
